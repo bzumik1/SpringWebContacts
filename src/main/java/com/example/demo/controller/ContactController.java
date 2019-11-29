809a532d7637cb319533282f5aabe5656981029e
Jakub Znamenáček <bzumik1@users.noreply.github.com>
2019-11-29 19:47:33 +0100

    FindById added

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -5,27 +5,36 @@ import com.example.demo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
+@RequestMapping("/test")
 @RestController
 public class ContactController {
 
     @Autowired
     ContactService contactService;
 
-    @GetMapping(path = "/test")
+    @GetMapping
     public List<Contact> getAllContacts(){
         return contactService.getAllContacts();
     }
 
-    @PostMapping(path = "/test")
+    @PostMapping
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
         return new ResponseEntity<>(contactService.createContact(contact), HttpStatus.ACCEPTED);
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable UUID id){
+        var temp = contactService.getContactById(id);
+        if(temp.isPresent()){
+            return new ResponseEntity<>(temp.get(),HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(temp.orElse(null),HttpStatus.NOT_FOUND);
+    }
+
 
 }
--- src/main/java/com/example/demo/service/ContactService.java
+++ src/main/java/com/example/demo/service/ContactService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ContactService {
@@ -22,6 +24,10 @@ public class ContactService {
         var temp = new ArrayList<Contact>();
         contactRepository.findAll().forEach(c -> temp.add(c));
         return temp;
+    }
+
+    public Optional<Contact> getContactById(UUID id){
+        return contactRepository.findById(id);
     }
 
 
